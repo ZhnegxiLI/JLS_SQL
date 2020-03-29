2698e6a8bb1924806da08a52131bdbf02b3561ba
martin <zhengxi.li.19@neoma-bs.com>
2020-03-30 00:00:34 +0200

    Remove the product data

---
+++ functional script/Remove all.sql
@@ -0,0 +1,17 @@
+delete from ReferenceLabel
+
+delete from ProductPhotoPath
+delete from ProductComment
+delete from Product
+
+delete from OrderInfoLog
+delete from OrderProduct
+delete from OrderInfoStatusLog
+delete from OrderInfo
+
+delete from Adress
+
+delete from ReferenceItem
+
+delete from ReferenceCategory
+GO
